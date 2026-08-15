9d90e9d5ea5ed5f91f6d4037fbc1396002a8b0b2
Nakulnk098 <nakulimpo@gmail.com>
2026-08-15 22:37:43 +0530

    TerraGuard: fix ingress drift on aws_security_group.demo_sg

--- infra/main.tf
+++ infra/main.tf
@@ -55,3 +55,4 @@ resource "aws_security_group" "demo_sg" {
     Project = "TerraGuard"
   }
 }
+# TODO(TerraGuard): RISKY drift on aws_security_group.demo_sg -- ingress changed -- see PR for details
